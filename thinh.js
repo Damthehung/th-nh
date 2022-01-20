514d817aa1e863d48ebf3c4849735551acf366d9
Damthehung <91153141+Damthehung@users.noreply.github.com>
2022-01-20 11:46:38 +0700

    Update and rename thính.txt to thinh.js

---
+++ thinh.js
@@ -0,0 +1,10 @@
+{
+    "data":{
+    "1":"Chớ nghe lời phỉnh tiếng phờ\nThò tay vào lờ, mắc kẹt cái hom",
+    "2":"Măng chua nấu cá ngạch nguồn\nSự đời đắp đổi, khi buồn khi vui.",
+    "3":"Yêu nhau xé lụa may quần\nGhét nhau kể nợ kể nần nhau ra",
+    "4":"Ai ơi chớ vội cười nhau\nCây nào mà chẳng có sâu chạm cành",
+    "5":"Khi giàu chẳng có đỡ ai\nĐến khi hoạn nạn chẳng ai đỡ mình",
+    "6":"Ở sao cho vừa lòng người\nỞ rộng người cười, ở hẹp người chê.",
+    "7":"Chim khôn chưa bắt đã bay,\nNgười khôn ít nói, ít hay trả lời"
+	}
